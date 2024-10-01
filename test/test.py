467b61d3054edc87ad0b606f3c87a7da6de047be
caianehardt <caianehardt@gmail.com>
2024-09-30 22:52:59 -0300

    add tests

--- test/test.py
+++ test/test.py
@@ -1,34 +1,34 @@
 from src.main import *
+import pytest
+import pytest_asyncio
 
-def test_root():
-    result = root()
-    yield result
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
     assert result == {"message": "Hello World"}
 
-
-def test_create_estudante():
+@pytest.mark.asyncio
+async def test_create_estudante():
     estudante_test = Estudante(name="Caiane Teste", curso="ADS", ativo=False)
-    result = create_estudante(estudante_test)
-    yield result
+    result = await create_estudante(estudante_test)
     assert estudante_test == result
 
-
-def test_update_estudante_negativo():
-    result = update_estudante(-5)
-    yield result
+@pytest.mark.asyncio
+async def test_update_estudante_negativo():
+    result = await update_estudante(-5)
     assert not result
 
-def test_update_estudante_positivo():
-    result = update_estudante(10)
-    yield result
+@pytest.mark.asyncio
+async def test_update_estudante_positivo():
+    result = await update_estudante(10)
     assert result
 
-def test_delete_estudante_negativo():
-    result = delete_estudante(-9)
-    yield result
+@pytest.mark.asyncio
+async def test_delete_estudante_negativo():
+    result = await delete_estudante(-9)
     assert result
 
-def test_delete_estudante_positivo():
-    result = delete_estudante(6)
-    yield result
+@pytest.mark.asyncio
+async def test_delete_estudante_positivo():
+    result = await delete_estudante(6)
     assert result
